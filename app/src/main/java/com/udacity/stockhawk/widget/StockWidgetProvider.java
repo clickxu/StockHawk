7e9c2b7ce91cd7a7d3996e17c25097f364b2f351
t-xu <t-xu@gmo-click.com>
2017-05-31 10:11:14 +0900

    update your widget when an item is added or removed

--- app/src/main/java/com/udacity/stockhawk/widget/StockWidgetProvider.java
+++ app/src/main/java/com/udacity/stockhawk/widget/StockWidgetProvider.java
@@ -3,6 +3,7 @@ package com.udacity.stockhawk.widget;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.widget.RemoteViews;
@@ -11,12 +12,15 @@ import com.udacity.stockhawk.R;
 import com.udacity.stockhawk.sync.QuoteIntentService;
 import com.udacity.stockhawk.ui.HistoryActivity;
 
+import static com.udacity.stockhawk.sync.QuoteSyncJob.ACTION_DATA_UPDATED;
+
 /**
  * Created by t-xu on 5/22/17.
  */
 
 public class StockWidgetProvider extends AppWidgetProvider {
 
+    @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         for (int appWidgetId : appWidgetIds) {
             updateStockList(context, appWidgetManager, appWidgetId);
@@ -41,14 +45,26 @@ public class StockWidgetProvider extends AppWidgetProvider {
         PendingIntent appPendingIntent = PendingIntent.getActivity(context, 0, appIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         remoteViews.setPendingIntentTemplate(R.id.stock_list, appPendingIntent);
 
+        Intent quoteIntent = new Intent(context, QuoteIntentService.class);
+        PendingIntent refreshPendingIntent = PendingIntent.getService(context, 0, quoteIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        remoteViews.setOnClickPendingIntent(R.id.no_data, refreshPendingIntent);
+
         //setting an empty view in case of no data
         remoteViews.setEmptyView(R.id.stock_list, R.id.no_data);
-        // Add the wateringservice click handler
-        Intent quoteIntent = new Intent(context, QuoteIntentService.class);
-        PendingIntent wateringPendingIntent = PendingIntent.getService(context, 0, quoteIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        remoteViews.setOnClickPendingIntent(R.id.no_data, wateringPendingIntent);
 
         appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
     }
 
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+
+        if (ACTION_DATA_UPDATED.equals(intent.getAction())) {
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+            ComponentName thisAppWidget = new ComponentName(context.getPackageName(),
+                    StockWidgetProvider.class.getName());
+            int[] appWidgetIds = appWidgetManager.getAppWidgetIds(thisAppWidget);
+            appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.stock_list);
+        }
+    }
 }
